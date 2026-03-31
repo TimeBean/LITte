78bbaaca59f8ee4913e3f7689da9b1ef43f11142
Alexander <alexanderklush8@gmail.com>
2026-03-31 18:16:20 +0300

    feat: add PageDownloader

---
+++ Litee.Engine/Service/PageDownloader.cs
@@ -0,0 +1,26 @@
+using Litee.Indexer.Model;
+
+namespace Litee.Indexer.Service;
+
+public class PageDownloader
+{
+    private readonly HttpClient _httpClient;
+
+    public PageDownloader(HttpClient httpClient)
+    {
+        _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+    }
+
+    public async Task<Page> GetPage(Url url, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(url);
+
+        using var request = new HttpRequestMessage(HttpMethod.Get, url.ToString());
+        using var response = await _httpClient
+            .SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+
+        var content = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+
+        return new Page(url, content);
+    }
+}
