f075e8983240de4a245541525275ded2441e76da
Alexander <alexanderklush8@gmail.com>
2026-03-31 20:44:25 +0300

    feat: Add Litte.ConsoleApp

---
+++ Litee.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Litee.Indexer.Model;
+using Litee.Indexer.Service;
+
+namespace Litee.ConsoleApp
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            var httpClient = new HttpClient();
+            var pageDownloader = new PageDownloader(httpClient);
+            var crawler = new Crawler(pageDownloader);
+            await crawler.Run(new Url("https://google.com"));
+        }
+    }
+}
